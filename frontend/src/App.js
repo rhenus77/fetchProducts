7fb5576cde164855d941dbfc78c2287108a950cd
rhenus77 <rhenuslokol13477@gmail.com>
2022-08-23 13:05:53 +0300

    Fetch-Products

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+// import data from './data';
+import axios from 'axios';
+
+function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="App">
+      <main>
+        <h1>Products</h1>
+        <div className="products">
+          {/* {data.products.map((product) => ( */}
+            {products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,33 @@
+// const data = {
+//     products: [
+//       {
+//         name: 'Mercedes',
+//         slug: 'mercedes',
+//         image: '/images/img1.jpg', 
+//         price: 120,
+        
+//       },
+//       {
+//         name: 'Mercedes S',
+//         slug: 'mercedes',
+//         image: '/images/img2.jpg', 
+//         price: 120,
+        
+//       },
+//       {
+//         name: 'Audi R8',
+//         slug: 'audi',
+//         image: '/images/img3.jpg', 
+//         price: 120,
+       
+//       },
+//       {
+//         name: 'Range Rover',
+//         slug: 'Range Rover',
+//         image: '/images/img4.jpg', 
+//         price: 120,
+        
+//       },
+//     ],
+//   };
+//   export default data;
